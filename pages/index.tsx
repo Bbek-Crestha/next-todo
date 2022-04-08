7b93a48dbe06bbdc5c337e880f8c4e0b4cbcc998
Bbek-Crestha <bcrestha07@gmail.com>
2022-04-08 20:56:20 +0545

    chore(tailwind): install tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
 	return (
 		<>
-			<h1>Hello World</h1>
+			<Head>
+				<title>To-Do</title>
+				<meta
+					name="description"
+					content="To-Do application created using Next.JS with user authentication.	"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main>
+				<h1 className="text-3xl text-red-500 text-center">Hello World</h1>
+			</main>
 		</>
 	);
 };
