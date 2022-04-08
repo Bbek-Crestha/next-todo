31122b05ba7113e3d5ffd40e2d97802f881c2e88
Bbek-Crestha <bcrestha07@gmail.com>
2022-04-08 22:26:12 +0545

    feat(database): create user type and user array

---
+++ database/Data.ts
@@ -0,0 +1,9 @@
+import { userType } from "./DataTypes";
+
+export const users: userType[] = [
+	{
+		name: "Admin",
+		email: "admin@test.com",
+		password: "admin123",
+	},
+];
---
+++ database/DataTypes.ts
@@ -0,0 +1,5 @@
+export type userType = {
+	name: string;
+	email: string;
+	password: string;
+};
