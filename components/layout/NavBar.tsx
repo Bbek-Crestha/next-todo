2746d1fe2bc90778fcce6dd3ed019f5635b1bea7
Bbek-Crestha <bcrestha07@gmail.com>
2022-04-09 12:55:15 +0545

    feat(navbar): create logout function

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,6 +1,17 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { destroyCookie } from "nookies";
 
 const NavBar = () => {
+	const router = useRouter();
+
+	const handleLogout = () => {
+		if (confirm("Are you sure to logout?")) {
+			destroyCookie(null, "token");
+			router.push("/login");
+		}
+	};
+
 	return (
 		<div className="bg-black w-[100%] absolute top-0 h-[50px] flex items-center px-6 py-4 justify-between">
 			<div className="flex items-center gap-2">
@@ -8,7 +19,9 @@ const NavBar = () => {
 				<span className="font-bold text-xl text-white">TO-DO</span>
 			</div>
 
-			<button className="text-xl text-white">Logout</button>
+			<button onClick={handleLogout} className="text-xl text-white">
+				Logout
+			</button>
 		</div>
 	);
 };
