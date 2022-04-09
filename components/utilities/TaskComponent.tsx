f0d0577b98901c0d0fb95ceea7876d21dc3e6ff1
Bbek-Crestha <bcrestha07@gmail.com>
2022-04-09 14:21:01 +0545

    feat(taskComponent): add handler to change task status

--- components/utilities/TaskComponent.tsx
+++ components/utilities/TaskComponent.tsx
@@ -1,18 +1,29 @@
 import { taskType } from "@database/DataTypes";
+import { ChangeEvent, useState } from "react";
 
 type Props = {
 	task: taskType;
 };
 
 const TaskComponent = (props: Props) => {
-	const {
-		task: { description, status },
-	} = props;
+	const { task } = props;
+	const [currentTask, setCurrentTask] = useState<taskType>(task);
+
+	const handleChange = (e: ChangeEvent) => {
+		setCurrentTask({ ...currentTask, status: !currentTask.status });
+	};
 
 	return (
-		<div className="flex items-center gap-2 px-4 mb-2">
-			<input type="checkbox" className="h-4 w-4 cursor-pointer" />
-			<p className="text-xl">{description}</p>
+		<div className="flex items-center gap-4 px-4 mb-2 text-xl">
+			<input
+				type="checkbox"
+				checked={currentTask.status}
+				onChange={handleChange}
+				className="h-4 w-4 cursor-pointer"
+			/>
+			<p className={currentTask.status ? "line-through text-gray-700" : ""}>
+				{currentTask.description}
+			</p>
 		</div>
 	);
 };
