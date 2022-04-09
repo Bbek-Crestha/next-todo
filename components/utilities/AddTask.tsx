562b1eadfb0458c91a0b23a3834482a3b11df058
Bbek-Crestha <bcrestha07@gmail.com>
2022-04-09 20:48:56 +0545

    feat(AddTask): create new component to add new task

---
+++ components/utilities/AddTask.tsx
@@ -0,0 +1,73 @@
+import { FormEvent, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import InputField from "./InputField";
+import { taskType } from "@database/DataTypes";
+
+type Props = {
+	tasks: taskType[];
+	setTasks: Function;
+};
+
+const AddTask = (props: Props) => {
+	const { tasks, setTasks } = props;
+	const [openField, setOpenField] = useState<boolean>(false);
+	const [task, setTask] = useState<string>("");
+
+	const handleAdd = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		if (task !== "") {
+			const newTask: taskType = {
+				id: uuidv4(),
+				description: task,
+				status: false,
+			};
+
+			setTasks([...tasks, newTask]);
+		}
+
+		setTask("");
+		setOpenField(false);
+	};
+
+	return (
+		<div className="text-center">
+			{openField ? (
+				<form onSubmit={handleAdd} className="flex justify-between">
+					<InputField
+						type="text"
+						placeholder="New Task"
+						value={task}
+						setValue={setTask}
+						margin={0}
+						autoFocus={true}
+					/>
+
+					<button
+						type="submit"
+						className="text-2xl font-bold bg-green-500 px-3 rounded-full"
+					>
+						+
+					</button>
+
+					<button
+						onClick={() => setOpenField(false)}
+						className="text-xl font-bold bg-red-500 px-3 rounded-full"
+					>
+						x
+					</button>
+				</form>
+			) : (
+				<button
+					onClick={() => setOpenField(true)}
+					className="bg-blue-500 rounded-lg py-1 px-4 mt-2 font-semibold"
+				>
+					Add new task
+				</button>
+			)}
+		</div>
+	);
+};
+
+export default AddTask;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { tasks as Tasks } from "@database/Data";
 import { taskType } from "@database/DataTypes";
 import NavBar from "@components/layout/NavBar";
 import TaskComponent from "@components/utilities/TaskComponent";
+import AddTask from "@components/utilities/AddTask";
 
 const Home: NextPage = () => {
 	const [tasks, setTasks] = useState<taskType[]>(Tasks);
@@ -26,11 +27,14 @@ const Home: NextPage = () => {
 
 				<div className="border-2 p-4 rounded-lg max-h-[calc(100vh-100px)] overflow-y-auto border-black shadow-black shadow-lg w-96 lg:w-1/3">
 					<p className="text-center text-2xl font-bold mb-4">Tasks</p>
+
 					{tasks.map((task) => (
 						<div key={task.id}>
 							<TaskComponent task={task} tasks={tasks} setTasks={setTasks} />
 						</div>
 					))}
+
+					<AddTask tasks={tasks} setTasks={setTasks} />
 				</div>
 			</main>
 		</>
