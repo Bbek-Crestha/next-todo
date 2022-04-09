410299eed3debf7198a26ab9edf129c591ed68f6
Bbek-Crestha <bcrestha07@gmail.com>
2022-04-09 19:51:44 +0545

    chore(uuid): install uuid for unique ids of users and tasks

--- database/Data.ts
+++ database/Data.ts
@@ -1,7 +1,10 @@
+import { v4 as uuidv4 } from "uuid";
+
 import { userType, taskType } from "@database/DataTypes";
 
 export const users: userType[] = [
 	{
+		id: uuidv4(),
 		name: "Admin",
 		email: "admin@test.com",
 		password: "admin123",
@@ -10,14 +13,17 @@ export const users: userType[] = [
 
 export const tasks: taskType[] = [
 	{
+		id: uuidv4(),
 		description: "100 side jumps",
 		status: true,
 	},
 	{
+		id: uuidv4(),
 		description: "20 pushups",
 		status: false,
 	},
 	{
+		id: uuidv4(),
 		description: "10km walk",
 		status: false,
 	},
--- database/DataTypes.ts
+++ database/DataTypes.ts
@@ -1,10 +1,12 @@
 export type userType = {
+	id: string;
 	name: string;
 	email: string;
 	password: string;
 };
 
 export type taskType = {
+	id: string;
 	description: string;
 	status: boolean;
 };
